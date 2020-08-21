b690410d7c72ad379b6d728ce230dbec4a8be00f
Jeffrey0710 <jeffreyhurtarte7@gmail.com>
2020-08-21 13:25:10 -0600

    Attached AddActivity(Add Screen) Implement Button "Add"

--- app/src/main/java/com/myapplication/BaseActivity.java
+++ app/src/main/java/com/myapplication/BaseActivity.java
@@ -2,7 +2,9 @@ package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+    }
+
+    public void Add_New(View view){
+        Intent addNew = new Intent(this, AddActivity.class);
+        startActivity(addNew);
     }
 }
